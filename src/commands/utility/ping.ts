3468fbdc94612694397ea437dda61276981f384e
Thomas Van <tinbolw@gmail.com>
2026-06-25 11:21:16 -0700

    add boilerplate files

---
+++ src/commands/utility/ping.ts
@@ -0,0 +1,13 @@
+import {
+    type ChatInputCommandInteraction,
+    SlashCommandBuilder
+} from 'discord.js';
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction: ChatInputCommandInteraction) {
+		await interaction.reply('Pong!');
+	},
+};
